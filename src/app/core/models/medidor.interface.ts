3c4235429149b9be71f457727f82879b0b946870
javAlexis <aivega2@espe.edu.ec>
2025-12-15 14:48:44 -0500

    feat: integracion modulo medidores con backend django real

--- src/app/core/models/medidor.interface.ts
+++ src/app/core/models/medidor.interface.ts
@@ -1,38 +1,14 @@
-// // Define los posibles estados de un medidor
-// export enum EstadoMedidor {
-// 	Asignado = 'Asignado',
-// 	EnBodega = 'En Bodega', // (En stock)
-// 	Mantenimiento = 'Mantenimiento',
-// }
-
-// // Define la estructura de un objeto Medidor
-// export interface Medidor {
-// 	id: number;
-// 	codigo: string; // Ej. "M-00123"
-// 	marca: string;
-// 	fechaInstalacion: Date | null;
-// 	estado: EstadoMedidor;
-
-// 	// Datos del socio al que está asignado (pueden ser nulos)
-// 	idSocioAsignado: number | null;
-// 	nombreSocioAsignado: string | null; // Denormalizado para la tabla
-// }
-
 import { Socio } from './socio.interface';
 
-/**
- * Define la estructura de datos de un Medidor en el frontend.
- * Basado en MedidorModel de Django.
- */
 export interface Medidor {
 	id: number;
-	socio: number; // Guardamos el ID del socio
+	// CAMBIO CLAVE: El backend manda 'socio_id', no 'socio'
+	socio_id: number;
 	codigo: string;
 	esta_activo: boolean;
 	observacion: string | null;
 	tiene_medidor_fisico: boolean;
 
-	// Campos adicionales (opcionales) para mostrar en la tabla
-	// (El servicio los "rellenará" al listar)
+	// OPCIONAL: Esto lo llenaremos nosotros en el frontend
 	socio_data?: Socio;
 }
